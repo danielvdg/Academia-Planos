a1ff8b2bb273b55c97847b5886c1fe8d9a751274
danielvdg <daniel.vdg@hotmail.com>
2021-07-21 08:58:38 -0300

    implementando as rotas gets e criando DAO

---
+++ src/Infra/db.js
@@ -0,0 +1,18 @@
+const Aula = require('../Models/Aulas')
+
+module.exports = db = [
+    aulas  = [
+        {
+        "modalidade": "luta",
+        "horario": "11:00",
+        "sala": 205,
+        "serie": "iniciante"
+      },
+      {
+        "modalidade": "pilate",
+        "horario": "11:30",
+        "sala": 305,
+        "serie": "avançado"
+      }
+    ]
+]
--- src/Models/Aulas.js
+++ src/Models/Aulas.js
@@ -1,15 +1,16 @@
 class Aula{
 
     constructor(modalidade,horario,sala,serie,tipo){
+      
         this._modalidade = modalidade;
         this._horario = horario;
         this._sala = sala;
-        this._serie = serie 
-        this._tipo = tipo
+        this._serie = serie;
+        this._tipo = tipo;
 
     }
     
-
+    
 }
 
 module.exports = Aula;
